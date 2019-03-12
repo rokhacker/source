823111d4ea1b6f013a092e539e68ce62392a1e1d
Thomas <rokhacker@gmail.com>
2019-03-12 17:05:25 +0900

    Create crontab.sh

---
+++ crontab.sh
@@ -0,0 +1,4 @@
+#* * * * * source ~/.bash_profile && cd /root/getnews/ && /bin/python /root/getnews/get_html_news.py
+0 8 * * * source ~/.bash_profile && cd /root/getnews/ && /bin/python /root/getnews/get_html_news.py
+0 17 * * * source ~/.bash_profile && cd /root/getnews/ && /bin/python /root/getnews/get_html_news.py
+0 20 * * * source ~/.bash_profile && cd /root/getnews/ && /bin/python /root/getnews/get_html_news.py
